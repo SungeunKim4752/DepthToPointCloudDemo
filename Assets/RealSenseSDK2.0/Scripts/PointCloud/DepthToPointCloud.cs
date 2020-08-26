e440d14cc88e26e17c253fc9ee6caa3d54b1ad9b
SungEun Kim <kimsung4752@naver.com>
2020-08-27 01:25:00 +0900

    :wrench: Add default demo

---
+++ Assets/RealSenseSDK2.0/Scripts/PointCloud/DepthToPointCloud.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Intel.RealSense;
+
+public class DepthToPointCloud : MonoBehaviour
+{
+    public Texture depthTexture;
+    public PointCloud pointCloud;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.pointCloud = new PointCloud();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void updatePointCloud(Frame frame)
+    {
+        Points points = pointCloud.Process(frame).As<Points>();
+        float[] vertices = new float[points.Count * 3];
+        points.CopyVertices(vertices);
+    }
+}
